254165faadd0646d6f8717d72a0297c5ef77adf2
Souliman <minasouliman@chevron.com>
2021-12-06 10:26:41 -0600

    added some nice reddit sols to day 4&5

--- 20211204/puzzle.py
+++ 20211204/puzzle.py
@@ -150,3 +150,18 @@ for i in range(len(draws)):
         called = np.isin(boards[~both], draws[:i])
         print(np.sum(boards[~both][~called]), draws[i - 1])
         break
+
+
+## cool solution found from reddit, saving it here--
+from numpy import loadtxt
+
+n, *b = open(0)
+
+n = loadtxt(n.split(",")).reshape(-1, 1, 1, 1)  # (numbers,1,1,1)
+b = loadtxt(b).reshape(1, -1, 5, 5)  # (1,boards,5,5)
+
+m = (n == b).cumsum(0)  # (numbers,boards,5,5)
+s = (n * b * (1 - m)).sum((2, 3))  # (numbers,boards)
+w = (m.all(2) | m.all(3)).any(2).argmax(0)  # (boards,)
+
+print(s[w].diagonal()[w.argsort()[[0, -1]]])
