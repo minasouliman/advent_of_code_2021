024035ae898cb08d718c4c5929089884a65214b6
Mina Souliman <minasouliman@gmail.com>
2021-12-13 20:24:36 -0600

    1212

--- 20211212/puzzle.py
+++ 20211212/puzzle.py
@@ -9,35 +9,35 @@ with open("input.csv", "r") as f:
     for i, line in enumerate(reader):
         k, v = line[0].split("-")
 
-        if (k in network) and (v != 'start'):
+        if (k in network) and (v != "start"):
             network[k].append(v)
-        if (v in network) and (k != 'start'):        
+        if (v in network) and (k != "start"):
             network[v].append(k)
-        if (k not in network) and (v != 'start'):
+        if (k not in network) and (v != "start"):
             network[k] = [v]
-        if (v not in network) and (k != 'start'):
+        if (v not in network) and (k != "start"):
             network[v] = [k]
-
 
 
 paths = []
 
 
-def traverse(node, path=None, parent_node = None):
-    if type(node) == list:
-         [traverse(n, path) for n in node]
-    elif node == "start":
-        return traverse(network[node], "start")
-    elif node not in network:
-        if node == "end":
-            paths.append(f"{path},end")
-        elif node.islower():
-            return None
-        else:
-            return traverse(network[node], f"{path},{network[node]}")
+def traverse(node, path=None, single_small=False):
+    if node == "start":
+        return traverse(network[node], "start", single_small)
+    elif node == "end":
+        paths.append(f"{path},end")
+    elif type(node) == list:
+        [traverse(n, path, single_small) for n in node]
+    elif node.islower() & ((path.count(node) > 0) & single_small):
+        return None
+    elif node.islower() & (path.count(node) == 1):
+        traverse(network[node], f"{path},{node}", True)
     else:
-        traverse(network[node], f"{path},{node}", node)
+        traverse(network[node], f"{path},{node}", single_small)
+
 
 print(network)
 traverse("start")
-# paths
+print(paths)
+len(paths)
