d8d0cf150f16d97330108aff1f11314b987f45ff
Mina Souliman <minasouliman@gmail.com>
2021-12-14 00:49:48 -0600

    whatever

--- 20211214/puzzle.py
+++ 20211214/puzzle.py
@@ -11,7 +11,7 @@ with open("input.csv", "r") as f:
         k, v = line[0].split(" -> ")
         insertions[k] = v
 
-pattern = "NNCB"
+pattern = "OOBFPNOPBHKCCVHOBCSO"
 pattern_dict = Counter()
 for j in range(len(pattern) - 1):
     pattern_dict[pattern[j : j + 2]] = 1
@@ -20,8 +20,17 @@ for j in range(len(pattern) - 1):
 for i in range(40):
     new_pattern_dict = Counter()
     for k in pattern_dict:
-        new_pattern_dict[k[0] + insertions[k]] = 1
-        new_pattern_dict[insertions[k] + k[1]] = 1
-    pattern_dict = pattern_dict + new_pattern_dict
+        new_pattern_dict[k[0] + insertions[k]] += pattern_dict[k]
+        new_pattern_dict[insertions[k] + k[1]] += pattern_dict[k]
+    pattern_dict = new_pattern_dict
 
-print(pattern_dict)
+first = Counter()
+second = Counter()
+for k in pattern_dict:
+    first[k[0]] += pattern_dict[k]
+    second[k[1]] += pattern_dict[k]
+
+print(first, second)
+
+print(first.most_common()[0][1], first.most_common()[-1][1])
+print(second.most_common()[0][1], second.most_common()[-1][1])
